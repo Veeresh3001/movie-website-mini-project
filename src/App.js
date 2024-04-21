df377d42af0889dfa09f23be841bf9db423757e5
Veeresh3001 <veeresh3001@gmail.com>
2024-04-21 21:59:22 +0530

    all files uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch, Redirect} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './components/Home'
+import Login from './components/Login'
+import Popular from './components/Popular'
+import NotFound from './components/NotFound'
+import Account from './components/Account'
+import Search from './components/Search'
+import MovieItemDetails from './components/MovieItemDetails'
+import UserProtect from './components/UserProtect'
 
-export default App;
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <UserProtect exact path="/" component={Home} />
+    <UserProtect exact path="/popular" component={Popular} />
+    <UserProtect exact path="/movies/:id" component={MovieItemDetails} />
+    <UserProtect exact path="/account" component={Account} />
+    <UserProtect exact path="/search" component={Search} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
